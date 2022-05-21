069490110bf3646f49494a2074868184c50a3bd2
ShayRedD12 <correoalexander2208@gmail.com>
2022-05-20 22:33:38 -0500

    ejemplo buble while-estructura de control

--- 12_bucles/index.php
+++ 12_bucles/index.php
@@ -1,3 +1,25 @@
 <?php
 //ESTRUCTURAS DE CONTROL- ITERADORES Y BUCLES
 
+//es una estructura de control que itera o repite la ejecucion de una serie de instrucciones muchas veces o tantas veces como sea necesario 
+//en base a una condicion que le buble lleva 
+
+//Bucle while 
+
+/*
+    while (condiciones){
+        bloque de instrucciones
+    
+    }
+
+*/
+
+//Ejemplo 1 Bucle While 
+
+$num = 0;
+while($num <= 100){
+
+    echo "<p>$num</p>";
+    $num++;
+
+}
